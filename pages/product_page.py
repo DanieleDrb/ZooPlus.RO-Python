60c791ac7e2e3a2dd8b1335c1eca852c4ef8654a
DanieleDrb <dany_find@yahoo.ro>
2024-05-02 01:04:52 +0300

    Added the assertion for the analysis section

--- pages/product_page.py
+++ pages/product_page.py
@@ -20,10 +20,17 @@ class ProductPage:
         self.scroll_until_element_visible(description_element)
         assert description_element.is_displayed(), "Description element is not visible"
 
-
-
-
-
+    def select_and_assert_of_analysis_section(self):
+    self.wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, "#constituents-header")
+    )).click()
+    analysis_element_1 = self.wait.until(EC.presence_of_element_located(
+        (By.XPATH, "//td[normalize-space()='proteine']")
+    )).text
+    analysis_element_2 = self.driver.find_element(
+        By.XPATH, "//td[contains(text(),'acizi graşi Omega-6')]").text
+    assert analysis_element_1 == "proteine", "Analysis section is not displayed as expected"
+    assert analysis_element_2 == "acizi graşi Omega-6", "Analysis section is not displayed as expected"
 
 
 
