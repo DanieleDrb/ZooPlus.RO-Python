84622f7e8f865f56f47128a87a1fad0eeef97489
DanieleDrb <dany_find@yahoo.ro>
2024-05-02 01:08:53 +0300

    Added second test for test product page

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -26,17 +26,30 @@ def product_page(setup_teardown):
     yield _product_page()
 
 
+@pytest.mark.priority(level=1)
 def test_presence_of_description_product(home_page, product_page):
     # Open the website and accept cookie
     home_page.open()
     home_page.cookie_button()
     # Go to the page product
-    home_page.input_food_name()
+    home_page.input_food_name("hrana uscata hills id")
     home_page.search_button()
-    home_page.selected_product()
+    home_page.selected_product_1()
     # Verify the presence of description in the product page
     product_page.assert_presence_of_description()
 
+
+@pytest.mark.priority(level=2)
+def test_presence_of_analysis_section(home_page, product_page):
+    # Open the website and accept cookie
+    home_page.open()
+    home_page.cookie_button()
+    # Go to the page product
+    home_page.input_food_name("brit")
+    home_page.search_button()
+    home_page.selected_product_2()
+    # Verify the presence of analysis section in the product page
+    product_page.select_and_assert_of_analysis_section()
 
 
 
