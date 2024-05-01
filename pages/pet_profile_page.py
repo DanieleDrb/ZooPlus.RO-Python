6b6db43b7e2a8cc55968982db4cc6422c0381acf
DanieleDrb <dany_find@yahoo.ro>
2024-05-02 01:10:35 +0300

    Update pet_profile_page.py

--- pages/pet_profile_page.py
+++ pages/pet_profile_page.py
@@ -1,3 +1,5 @@
+import time
+import pyautogui
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -45,6 +47,36 @@ class PetProfilePage:
             (By.XPATH, "//a[contains(text(),'Editează datele animăluțului')]")
         )).click()
 
+    def add_image(self):
+        self.wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR,
+             ".Picture-module__galleryClickable--cW9OntkJEC > div:nth-child(1) > button:nth-child(1) > svg:nth-child(1)")
+        )).click()
+
+        self.wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, ".PictureEdit-module__addPictureButton--_jfcoHF9d9")
+        )).click()
+
+        image_path = "C:\\Users\\dany_\\Interfata\\Desktop\\Open\\wego\\German Shepherd Dog1.jpg"
+        time.sleep(1)
+        pyautogui.write(image_path)
+        time.sleep(1)
+        pyautogui.press('enter')
+        time.sleep(1)
+
+    def save_button(self):
+        self.wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#mpp-form-terms-agreement")
+        )).click()
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Salvare']").click()
+
+    def assert_presence_of_image(self):
+        profile_image = self.wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR,
+             "div[class='Picture-module__squareImageGallery--wLYFmweUz6 Picture-module__squareImage--zHS_x6hwGC'] img")
+        ))
+        assert profile_image.is_displayed()
+    
     def delete_pet(self):
         self.wait.until(EC.element_to_be_clickable(
             (By.XPATH, "//button[contains(text(),'Șterge')]")
